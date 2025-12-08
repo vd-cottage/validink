75f0cd988d877e8415e7a1bdd441f6ed6cc7c06c
vd-cottage <vdcottageofficial@gmail.com>
2025-12-08 18:23:25 +0530

    Remove API rewrites configuration from next.config.js.

--- next.config.js
+++ next.config.js
@@ -19,14 +19,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: false,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/v1/:path*',
-        destination: 'http://localhost:4000/api/v1/:path*',
-      },
-    ]
-  },
+
 }
 
 module.exports = nextConfig
